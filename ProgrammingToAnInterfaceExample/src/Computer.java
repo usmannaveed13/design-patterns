9d182df7d6c1f52851e19f8ea547e6225172a2d7
usmannaveed13 <usmannaveed13@gmail.com>
2022-12-16 12:43:15 +0500

    interface example

---
+++ ProgrammingToAnInterfaceExample/src/Computer.java
@@ -0,0 +1,23 @@
+public class Computer {
+    displayModule dm;
+
+    public void setDisplayModule(displayModule dm){
+        this.dm = dm;
+    }
+
+    public void display(){
+        dm.display();
+    }
+
+    public static void main(String[] args) {
+       Computer cm = new Computer();
+       displayModule dm = new Monitor();
+       displayModule dm1 = new Projector();
+
+       cm.setDisplayModule(dm);
+       cm.display();
+
+       cm.setDisplayModule(dm1);
+       cm.display();
+    }
+}
---
+++ ProgrammingToAnInterfaceExample/src/displayModule.java
@@ -0,0 +1,14 @@
+public interface displayModule {
+    public void display();
+}
+
+class Monitor implements displayModule {
+    public void display() {
+          System.out.println("display through Monitor");
+    }
+}
+class Projector implements displayModule {
+    public void display() {
+        System.out.println("display through projector");
+    }
+}
