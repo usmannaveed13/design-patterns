5de76d2211ed2c5c99069ee771258e7b7eb9f339
Usman Naveed <usmna.naveed@2iqresearch.com>
2023-01-06 14:41:14 +0500

    Design pattern (Adapter TurkeyDuck Example)

---
+++ designPatterns/src/structuralPatterns/adapter/objectAdapterTurkeyDuckExample/Duck.java
@@ -0,0 +1,18 @@
+package structuralPatterns.adapter.objectAdapterTurkeyDuckExample;
+
+public interface Duck {
+    public void quack();
+    public void fly();
+}
+
+class MallardDuck implements Duck {
+    @Override
+    public void quack() {
+        System.out.println("Quack");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("I am flying");
+    }
+}
