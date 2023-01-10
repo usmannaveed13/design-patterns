7527e9fde9c37bb64ce1b55205c9baaa74f19759
Usman Naveed <usmna.naveed@2iqresearch.com>
2023-01-10 18:20:40 +0500

    Design pattern (Proxy example)

---
+++ designPatterns/src/structuralPatterns/proxy/Client.java
@@ -0,0 +1,13 @@
+package structuralPatterns.proxy;
+
+public class Client {
+    public static void main(String [] args) {
+        Image image  = new ProxyImage("someFilename.jpg");
+
+        // image will be loaded from disk
+        image.display();
+        System.out.println("");
+
+        image.display();
+    }
+}
---
+++ designPatterns/src/structuralPatterns/proxy/Image.java
@@ -0,0 +1,25 @@
+package structuralPatterns.proxy;
+
+public interface Image {
+    void display();
+}
+
+class RealImage implements Image {
+    private String fileName;
+
+    public RealImage(String fileName) {
+        this.fileName = fileName;
+        loadFromDisk(this.fileName);
+    }
+
+    @Override
+    public void display()
+    {
+        System.out.println("Displaying " + fileName);
+    }
+
+    private void loadFromDisk(String fileName) {
+        System.out.println("Loading " + fileName);
+    }
+}
+
