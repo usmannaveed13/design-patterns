b9b461562f51d9858760a0e14d40dce16dbc430e
Usman Naveed <usmna.naveed@2iqresearch.com>
2023-01-11 17:07:58 +0500

    Design pattern (interpreter example)

---
+++ designPatterns/src/behavioralPatterns/interpreter/Expression.java
@@ -0,0 +1,32 @@
+package behavioralPatterns.interpreter;
+
+public interface Expression {
+    String interpret(InterpreterContext ic);
+}
+
+class IntToBinaryExpression implements Expression {
+    private int i;
+
+    public IntToBinaryExpression(int c)
+    {
+        i = c;
+    }
+
+    @Override
+    public String interpret(InterpreterContext ic) {
+        return ic.getBinaryFormat(i);
+    }
+}
+
+class IntToHexExpression implements Expression {
+    private int i;
+
+    public IntToHexExpression(int c){
+        this.i=c;
+    }
+
+    @Override
+    public String interpret(InterpreterContext ic) {
+        return ic.getHexadecimalFormat(i);
+    }
+}
---
+++ designPatterns/src/behavioralPatterns/interpreter/InterpreterContext.java
@@ -0,0 +1,11 @@
+package behavioralPatterns.interpreter;
+
+public class InterpreterContext {
+    public String getBinaryFormat(int i) {
+        return Integer.toBinaryString(i);
+    }
+
+    public String getHexadecimalFormat(int i) {
+        return Integer.toHexString(i);
+    }
+}
