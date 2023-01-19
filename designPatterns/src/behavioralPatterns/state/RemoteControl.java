539fff412a50a5dd1b67ba27e9c4d4454ccc75d2
Usman Naveed <usmna.naveed@2iqresearch.com>
2023-01-19 10:29:54 +0500

    Design pattern (state example)

---
+++ designPatterns/src/behavioralPatterns/state/RemoteControl.java
@@ -0,0 +1,22 @@
+package behavioralPatterns.state;
+
+abstract class RemoteControl {
+    public abstract void pressSwitch(TV context);
+}
+
+class On extends RemoteControl {
+    @Override
+    public void pressSwitch(TV context) {
+        System.out.println("I am already On.  Going to be Off now");
+        context.setState(new Off());
+    }
+}
+
+class Off extends RemoteControl
+{
+    @Override
+    public void pressSwitch(TV context){
+        System.out.println("I am Off.  Going to be On now");
+        context.setState(new On());
+    }
+}
