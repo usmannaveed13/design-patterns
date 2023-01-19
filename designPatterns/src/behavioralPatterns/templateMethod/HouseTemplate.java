45bdd4ec97b1fbc9d0b12c104d50b3c09dd0d7fb
Usman Naveed <usmna.naveed@2iqresearch.com>
2023-01-19 17:32:50 +0500

    Design pattern (TemplateMethod example)

---
+++ designPatterns/src/behavioralPatterns/templateMethod/HouseTemplate.java
@@ -0,0 +1,50 @@
+package behavioralPatterns.templateMethod;
+
+abstract class HouseTemplate {
+
+    // template method, final so subclasses cannot override it
+    public final void buildHouse() {
+        buildFoundation();
+        buildPillars();
+        buildWalls();
+        buildWindows();
+        System.out.println("House is built");
+    }
+
+    // default implementation, hook method
+    private void buildWindows() {
+        System.out.println("Building Glass Windows");
+    }
+
+    //methods to be implemented by subclasses
+    public abstract void buildWalls();
+    public abstract void buildPillars();
+
+    private void buildFoundation() {
+        System.out.println("Building foundation with cement, iron rods and sand");
+    }
+}
+
+class WoodenHouse extends HouseTemplate {
+    @Override
+    public void buildWalls() {
+        System.out.println("Building Wooden Walls");
+    }
+
+    @Override
+    public void buildPillars() {
+        System.out.println("Building Pillars with Wood coating");
+    }
+}
+
+class GlassHouse extends HouseTemplate {
+    @Override
+    public void buildWalls() {
+        System.out.println("Building Glass Walls");
+    }
+
+    @Override
+    public void buildPillars() {
+        System.out.println("Building Pillars with glass coating");
+    }
+}
